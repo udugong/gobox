bb0344380722903a4f71fa78e526e11d92bd6ac7
joil <joil-z@outlook.com>
2025-03-04 11:38:14 +0800

    feat: 修改 kafka-gox 接口定义

--- kafka-gox/types.go
+++ kafka-gox/types.go
@@ -1,12 +1,6 @@
 package kafkax
 
-import (
-	"context"
-
-	"github.com/segmentio/kafka-go"
-)
-
-type Reader interface {
-	ReadMsg(ctx context.Context, r *kafka.Reader) error
+type Consumer interface {
+	Start() error
 	Close()
 }
